6a1cb04268dbf5944344b9e68df90466cb71df97
krishisunlau <145789812+krishisunlau@users.noreply.github.com>
2023-10-01 20:33:43 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+
+const app = document.getElementById("app");
+const root = ReactDOM.createRoot(app)
+root.render(<App />)
